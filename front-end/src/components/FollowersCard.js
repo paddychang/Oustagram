84829c26baad65f938a3ec5a9f317511f74096ed
Paddy <paddychang@gmail.com>
2021-12-21 18:07:16 +1100

    fixed unfollow dailog wrong name bug

--- front-end/src/components/FollowersCard.js
+++ front-end/src/components/FollowersCard.js
@@ -1,7 +1,3 @@
-import { useState } from "react";
-// Redux
-import { useSelector, useDispatch } from "react-redux";
-import { setUnfollowed } from "redux/actions/userActions";
 // MUI
 import {
   Box,
@@ -13,75 +9,53 @@ import {
   DialogActions,
 } from "@mui/material";
 
-const FollowersCard = () => {
-  const [open, setOpen] = useState(false);
-  const followers = useSelector((state) => state.user.followers);
-  const dispatch = useDispatch();
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handelUnfollow = (follower) => {
-    dispatch(setUnfollowed(follower));
-    setOpen(false);
-  };
-
+const FollowersCard = ({
+  open,
+  data,
+  handleClose,
+  handleOpen,
+  handelUnfollow,
+  follower,
+}) => {
   return (
-    <>
-      {followers.length > 0
-        ? followers.map((data) => (
-            <Box
-              key={data.follower}
-              sx={{ display: "flex", alignItems: "center", mb: 1, mt: 3 }}
-            >
-              <Avatar
-                aria-label="recipe"
-                src={data.followerImage}
-                sx={{ width: 60, height: 60 }}
-              />
-              <Box
-                component="div"
-                sx={{
-                  ml: 3,
-                  display: "flex",
-                  justifyContent: "space-between",
-                  mb: 0.5,
-                }}
-              >
-                <Typography variant="h1" sx={{ width: 220 }}>
-                  {data.follower}
-                </Typography>
-                <Button onClick={handleOpen} color="error" size="small">
-                  Unfollow
-                </Button>
-                <Dialog
-                  open={open}
-                  onClose={handleClose}
-                  fullWidth
-                  maxWidth="sm"
-                >
-                  <DialogTitle>{`Do you want to unfollow ${data.follower} ?`}</DialogTitle>
-                  <DialogActions>
-                    <Button onClick={handleClose} color="info">
-                      Cancel
-                    </Button>
-                    <Button
-                      onClick={() => handelUnfollow(data.follower)}
-                      color="error"
-                    >
-                      Unfollow
-                    </Button>
-                  </DialogActions>
-                </Dialog>
-              </Box>
-            </Box>
-          ))
-        : null}
-    </>
+    <Box sx={{ display: "flex", alignItems: "center", mb: 1, mt: 3 }}>
+      <Avatar
+        aria-label="recipe"
+        src={data.followerImage}
+        sx={{ width: 60, height: 60 }}
+      />
+      <Box
+        component="div"
+        sx={{
+          ml: 3,
+          display: "flex",
+          justifyContent: "space-between",
+          mb: 0.5,
+        }}
+      >
+        <Typography variant="h1" sx={{ width: 220 }}>
+          {data.follower}
+        </Typography>
+        <Button
+          onClick={() => handleOpen(data.follower)}
+          color="error"
+          size="small"
+        >
+          Unfollow
+        </Button>
+        <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+          <DialogTitle>{`Do you want to unfollow ${follower} ?`}</DialogTitle>
+          <DialogActions>
+            <Button onClick={handleClose} color="info">
+              Cancel
+            </Button>
+            <Button onClick={() => handelUnfollow(follower)} color="error">
+              Unfollow
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </Box>
   );
 };
 
--- front-end/src/components/ProfileCard.js
+++ front-end/src/components/ProfileCard.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 // Redux
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setUnfollowed } from "redux/actions/userActions";
 // MUI
 import { Box, Divider, Typography, Avatar } from "@mui/material";
 // Components
@@ -8,10 +10,27 @@ import FollowersCard from "./FollowersCard";
 import Copyright from "components/Copyright";
 
 const ProfileCard = () => {
+  const [open, setOpen] = useState(false);
+  const [follower, setFollower] = useState("");
+  // const followers = useSelector((state) => state.user.followers);
   const {
     credentials: { handle, imageUrl, location },
+    followers,
   } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
+  const handleOpen = (follower) => {
+    setFollower(follower);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handelUnfollow = () => {
+    dispatch(setUnfollowed(follower));
+    setOpen(false);
+  };
   return (
     <>
       <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
@@ -32,7 +51,19 @@ const ProfileCard = () => {
         </Box>
       </Box>
       <Divider>Followers</Divider>
-      <FollowersCard />
+      {followers.length > 0
+        ? followers.map((data) => (
+            <FollowersCard
+              key={data.follower}
+              data={data}
+              open={open}
+              handleClose={handleClose}
+              handleOpen={handleOpen}
+              handelUnfollow={handelUnfollow}
+              follower={follower}
+            />
+          ))
+        : null}
       <Copyright />
     </>
   );
